36c13453136c6aab0d8c4a6e8c0bc3b80e9c8339
Xansher <grzesiekaszlar@gmail.com>
2021-04-11 20:31:09 +0200

    Created experience controller

---
+++ MyPortfolioAPI/Controllers/ExperiencesController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.Controllers
+{
+    [ApiController]
+    [Route("api/experiences")]
+    public class ExperiencesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public ExperiencesController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+    }
+}
