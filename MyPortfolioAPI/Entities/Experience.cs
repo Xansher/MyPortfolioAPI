4278daf6f4d410235eced00f3d44348903281053
Xansher <grzesiekaszlar@gmail.com>
2021-04-11 20:19:24 +0200

    Created model

---
+++ MyPortfolioAPI/Entities/Experience.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.Entities
+{
+    public class Experience
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        [Required]
+        public string Position { get; set; }
+        [Required]
+        public string Company { get; set; }
+        public string EnglishDuties { get; set; }
+        public string PolishDuties { get; set; }
+    }
+}
