86374a320d6907881e05aded0ca7b791c6c54e34
Xansher <grzesiekaszlar@gmail.com>
2021-04-11 20:34:41 +0200

    Created model

---
+++ MyPortfolioAPI/Entities/AboutMe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.Entities
+{
+    public class AboutMe
+    {
+        public int Id { get; set; }
+        public string EnglishDescription { get; set; }
+        public string PolishDescription { get; set; }
+        public string Photo { get; set; }
+        
+    }
+}
