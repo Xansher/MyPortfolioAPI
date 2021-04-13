cf4f06bd000b8acce7b8894ea3a04748685ad062
Xansher <grzesiekaszlar@gmail.com>
2021-04-13 21:36:56 +0200

    Created about me creating dto

---
+++ MyPortfolioAPI/DTOs/AboutMeCreatingDTO.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.DTOs
+{
+    public class AboutMeCreatingDTO
+    {
+        public string EnglishDescription { get; set; }
+        public string PolishDescription { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
