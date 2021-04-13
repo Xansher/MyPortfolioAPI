c1c897148f8bc872fe5327c4bca6cdc3094b45e9
Xansher <grzesiekaszlar@gmail.com>
2021-04-13 21:36:47 +0200

    Create aboutme dto

---
+++ MyPortfolioAPI/DTOs/AboutMeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.DTOs
+{
+    public class AboutMeDTO
+    {
+        public int Id { get; set; }
+        public string EnglishDescription { get; set; }
+        public string PolishDescription { get; set; }
+        public string Photo { get; set; }
+    }
+}
