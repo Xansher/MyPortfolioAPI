bf611d3fbc12d0005a3673763cdfe68ffddcd580
Xansher <grzesiekaszlar@gmail.com>
2021-04-13 18:22:50 +0200

    Created experienceDTO

---
+++ MyPortfolioAPI/DTOs/ExperienceDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.DTOs
+{
+    public class ExperienceDTO
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Position { get; set; }
+        public string Company { get; set; }
+        public string EnglishDuties { get; set; }
+        public string PolishDuties { get; set; }
+    }
+}
