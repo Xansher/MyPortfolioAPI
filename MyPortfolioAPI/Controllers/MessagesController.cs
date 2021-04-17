6508f12d6dd09f3082511deb786734563b35331b
Xansher <grzesiekaszlar@gmail.com>
2021-04-17 19:04:59 +0200

    Created messages controller

---
+++ MyPortfolioAPI/Controllers/MessagesController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolioAPI.DTOs;
+using MyPortfolioAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.Controllers
+{
+    [Route("api/messages")]
+    [ApiController]
+    public class MessagesController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public MessagesController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] MessageCreatingDTO messageCreatingDTO)
+        {
+            var message = mapper.Map<Message>(messageCreatingDTO);
+            message.Date = DateTime.Now;
+            context.Add(message);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
