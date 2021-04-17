5fe2c665831b098a50482db2f6b597136258648c
Xansher <grzesiekaszlar@gmail.com>
2021-04-17 19:04:05 +0200

    Created contact entity

---
+++ MyPortfolioAPI/Entities/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Linkedin { get; set; }
+        public string Github { get; set; }
+        public string Instagram { get; set; }
+        public string Email { get; set; }
+    }
+}
