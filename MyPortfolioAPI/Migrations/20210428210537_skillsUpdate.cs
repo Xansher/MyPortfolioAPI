fda6e590057a50a2f6c85ea913215c6f846deed1
Xansher <grzesiekaszlar@gmail.com>
2021-04-29 00:39:49 +0200

    Added skills update

---
+++ MyPortfolioAPI/Migrations/20210428210537_skillsUpdate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyPortfolioAPI.Migrations
+{
+    public partial class skillsUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Skills",
+                newName: "PolishDescription");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EnglishDescription",
+                table: "Skills",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnglishDescription",
+                table: "Skills");
+
+            migrationBuilder.RenameColumn(
+                name: "PolishDescription",
+                table: "Skills",
+                newName: "Description");
+        }
+    }
+}
