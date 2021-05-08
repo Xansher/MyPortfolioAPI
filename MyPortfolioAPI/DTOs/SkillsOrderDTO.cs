87ebe1d69bbaa8ee274e8afed9f1ff7e0437347a
Xansher <grzesiekaszlar@gmail.com>
2021-05-08 19:38:31 +0200

    Creted skills order dto

---
+++ MyPortfolioAPI/DTOs/SkillsOrderDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioAPI.DTOs
+{
+    public class SkillOrderDTO
+    {
+        public int SkillId { get; set; }
+        public int Order { get; set; }
+    }
+}
